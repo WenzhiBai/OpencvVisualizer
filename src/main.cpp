43094a53ce9a51a7154164cd834c55c24373f606
Tank <tank.bai@qq.com>
2018-10-28 11:00:53 +0800

    完成2d可视化部分。

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,109 @@
 ﻿#include "opencv2/opencv.hpp"
+#include <Windows.h>
+#include <gl/GL.h>
+
+#define PI			3.1415926535
+#define WIDTH		800
+#define HEIGHT		800
+#define SCALE_STEP	0.1f
 
 using namespace cv;
 
-#define WIDTH 800
-#define HEIGHT 800
-#define SCALE_STEP 0.1f
+/* 2d visualization */
+float gScale2d = 1;
 
-float resizeRate = 1;
+Point gTargetPoint;
+Rect gRoiRect2d(0, 0, WIDTH, HEIGHT);
 
-Point targetPoint;
-Rect roiRect(0, 0, WIDTH, HEIGHT);
+Mat gSrcImg(Size(WIDTH, HEIGHT), CV_8UC3, Scalar(100, 100, 100));
+Mat gScaleImg(Size(WIDTH, HEIGHT), CV_8UC3, Scalar(100, 100, 100));
+Mat gResultImg(Size(WIDTH, HEIGHT), CV_8UC3, Scalar(100, 100, 100));
 
-Mat srcImg(Size(WIDTH, HEIGHT), CV_8UC3, Scalar(100, 100, 100));
-Mat scaleImg(Size(WIDTH, HEIGHT), CV_8UC3, Scalar(100, 100, 100));
-Mat resultImg(Size(WIDTH, HEIGHT), CV_8UC3, Scalar(100, 100, 100));
+/**
+  * 绘制十字
+  * @param[in] img 目标图像
+  * @param[in] point 十字中心点
+  * @param[in] color 颜色
+  * @param[in] size 十字尺寸
+  * @param[in] thickness 粗细
+  */
+void drawCross(Mat img, CvPoint point, CvScalar color, int size, int thickness)
+{
+	//绘制横线
+	line(img, Point(point.x - size / 2, point.y), Point(point.x + size / 2, point.y), color, thickness, 8, 0);
+	//绘制竖线
+	line(img, Point(point.x, point.y - size / 2), Point(point.x, point.y + size / 2), color, thickness, 8, 0);
+}
 
-void on_mouse(int event, int x, int y, int flags, void* userdata)
+void Update2d()
+{
+	resize(gSrcImg, gScaleImg, Size(gSrcImg.cols*gScale2d, gSrcImg.rows*gScale2d), 0, 0, InterpolationFlags::INTER_AREA);
+
+	Mat transformMat = Mat::zeros(2, 3, CV_32FC1);		//定义仿射变形矩阵
+	transformMat.at<float>(0, 0) = 1;
+	transformMat.at<float>(0, 2) = gRoiRect2d.x; //水平平移量
+	transformMat.at<float>(1, 1) = 1;
+	transformMat.at<float>(1, 2) = gRoiRect2d.y; //竖直平移量
+
+	warpAffine(gScaleImg, gResultImg, transformMat, gResultImg.size());//根据平移矩阵进行仿射变换
+
+	char text[128];
+	sprintf(text, "ROI RECT X = %d, Y = %d", gRoiRect2d.x, gRoiRect2d.y);
+	putText(gResultImg, text, cv::Point(50, 50), cv::FONT_HERSHEY_COMPLEX, 0.5, Scalar(0, 255, 255));
+
+	imshow("2d visualizer", gResultImg);
+}
+void OnMouse2d(int event, int x, int y, int flags, void* userdata)
 {
 	if (x < 0 || x > WIDTH - 1 || y < 0 || y > HEIGHT - 1)
 		return;
 
-	static int start_x = 0;
-	static int start_y = 0;
-	static Point ld = Point(0, 0);
-	static Point ru = Point(0, 0);
+	static int startRoiX = 0;
+	static int startRoiY = 0;
+	static Point startPoint = Point(0, 0);
 
+	//左键按下，记录开始移动时的位置
 	if (event == CV_EVENT_LBUTTONDOWN) {
-		ld = Point(x, y);
-		start_x = roiRect.x;
-		start_y = roiRect.y;
+		startPoint = Point(x, y);
+		startRoiX = gRoiRect2d.x;
+		startRoiY = gRoiRect2d.y;
 	}
+
+	//左键按下且鼠标移动中，记录移动的距离
 	if (event == CV_EVENT_MOUSEMOVE && (flags & CV_EVENT_LBUTTONDOWN)) {
-		int dx = x - ld.x;
-		int dy = y - ld.y;
-		roiRect.x = start_x + dx;
-		roiRect.y = start_y + dy;
+		int dx = x - startPoint.x;
+		int dy = y - startPoint.y;
+		gRoiRect2d.x = startRoiX + dx;
+		gRoiRect2d.y = startRoiY + dy;
 	}
+
+	//右键按下，在图像中画一个点
 	if (event == CV_EVENT_RBUTTONDOWN) {
-		targetPoint.x = (float)(x - roiRect.x) / resizeRate;
-		targetPoint.y = (float)(y - roiRect.y) / resizeRate;
-		//circle(scaleImg, Point(x, y), 1, Scalar(0, 0, 255), 4);
-		circle(srcImg, Point(targetPoint.x, targetPoint.y), 1, Scalar(0, 0, 255), 4);
+		gTargetPoint.x = (float)(x - gRoiRect2d.x) / gScale2d;
+		gTargetPoint.y = (float)(y - gRoiRect2d.y) / gScale2d;
+		circle(gSrcImg, Point(gTargetPoint.x, gTargetPoint.y), 1, Scalar(0, 0, 255), 4);
 	}
+
+	//滚轮滚动，对图像进行缩放
 	if (event == CV_EVENT_MOUSEWHEEL) {
 		int value = getMouseWheelDelta(flags);
-		float scaleStep = SCALE_STEP;
+		float scaleStep = 0;
 		if (value > 0)
 			scaleStep = SCALE_STEP;
 		else if (value < 0)
 			scaleStep = -SCALE_STEP;
-		resizeRate *= (1 + scaleStep);
-		resize(srcImg, scaleImg, Size(srcImg.cols*resizeRate, srcImg.rows*resizeRate), 0, 0, InterpolationFlags::INTER_AREA);
-		roiRect.x = x + (roiRect.x - x)*(1 + scaleStep);
-		roiRect.y = y + (roiRect.y - y)*(1 + scaleStep);
+		gScale2d *= (1 + scaleStep);
+		gRoiRect2d.x = x + (gRoiRect2d.x - x)*(1 + scaleStep);
+		gRoiRect2d.y = y + (gRoiRect2d.y - y)*(1 + scaleStep);
 	}
 
-	Mat t_mat = Mat::zeros(2, 3, CV_32FC1);		//定义平移矩阵
-	t_mat.at<float>(0, 0) = 1;
-	t_mat.at<float>(0, 2) = roiRect.x; //水平平移量
-	t_mat.at<float>(1, 1) = 1;
-	t_mat.at<float>(1, 2) = roiRect.y; //竖直平移量
-
-	warpAffine(scaleImg, resultImg, t_mat, resultImg.size());//根据平移矩阵进行仿射变换
-
-	char text[256];
-	sprintf(text, "ROI RECT X = %d, Y = %d", roiRect.x, roiRect.y);
-	putText(resultImg, text, cv::Point(50, 50), cv::FONT_HERSHEY_COMPLEX, 0.5, Scalar(0, 255, 255));
-
-	imshow("test", resultImg);
+	Update2d();
 }
 
 int main()
 {
-	namedWindow("test", CV_WINDOW_AUTOSIZE);
-	setMouseCallback("test", on_mouse);
+	namedWindow("2d visualizer", CV_WINDOW_AUTOSIZE);
+	setMouseCallback("2d visualizer", OnMouse2d);
 
 	while (true) {
 		waitKey(2);
